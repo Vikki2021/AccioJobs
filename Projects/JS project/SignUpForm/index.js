fe892b631a181ec563d3408c8675669bf7cd9eda
Vikas Kumar <vk972832@gmail.com>
2025-04-05 22:37:14 +0530

    "add opps"

---
+++ Projects/JS project/SignUpForm/index.js
@@ -0,0 +1,14 @@
+function checkEmail() {
+    let email = document.getElementById("email").value;
+    
+}
+
+function checkPassword() {
+    let password = document.getElementById("password").value;
+}
+
+
+function allGoodToGo() {
+    let email = document.getElementById("email").value;
+
+}
